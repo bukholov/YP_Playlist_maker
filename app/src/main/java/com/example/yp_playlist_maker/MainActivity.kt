8bebdf2d0110c9f211cb8fdf14fa9ef213cedec4
bukholovds <bukholovds@gmail.com>
2023-10-26 23:29:48 +0300

    Домашнее задание "Верстка экранов" * Подогнал размеры и шрифты из шаблонов; * Создал пустые экраны поиска и медиа;

--- app/src/main/java/com/example/yp_playlist_maker/MainActivity.kt
+++ app/src/main/java/com/example/yp_playlist_maker/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.yp_playlist_maker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.buttonSearch)
+        buttonSearch.setOnClickListener{startActivity(Intent(this, SearchActivity::class.java)) }
+
+        val buttonMedia = findViewById<Button>(R.id.buttonMedia)
+        buttonMedia.setOnClickListener { startActivity(Intent(this, MediaActivity::class.java))}
+
+        val buttonSettings = findViewById<Button>(R.id.buttonSettings)
+        buttonSettings.setOnClickListener{startActivity(Intent(this, SettingsActivity::class.java)) }
     }
 }
--- app/src/main/java/com/example/yp_playlist_maker/SettingsActivity.kt
+++ app/src/main/java/com/example/yp_playlist_maker/SettingsActivity.kt
@@ -2,10 +2,17 @@ package com.example.yp_playlist_maker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val buttonBackFromSettings = findViewById<Button>(R.id.buttonBackFromSettings)
+
+        buttonBackFromSettings.setOnClickListener{
+            finish()
+        }
     }
 }
