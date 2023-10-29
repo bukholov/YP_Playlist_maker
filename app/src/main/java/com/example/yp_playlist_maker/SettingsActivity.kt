5c62447824a1b3db7632dcc4c1467b90b9f47d5a
bukholovds <bukholovds@gmail.com>
2023-10-29 14:35:23 +0300

    Домашнее задание "Верстка экранов". Исправление замечений * Вынес оставшиеся цвета и размеры в ресурсы; * На экране настроек элементы меню в Button в TextView; * Исправил отступы между иконками и текстом кнопок на главном экране на идентичные в шаблоне Figma; * Использвал во всех размерах значения кратные 4; * Отсупы между кнопками на главном экране сделал идентичными шаблону; * Надпись "Playlist maker" на главном экране и кнопка "Настройки" на экране настроек выравнил идентично шаблону и использвал для этого общие ресурсы;

--- app/src/main/java/com/example/yp_playlist_maker/SettingsActivity.kt
+++ app/src/main/java/com/example/yp_playlist_maker/SettingsActivity.kt
@@ -2,15 +2,14 @@ package com.example.yp_playlist_maker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val buttonBackFromSettings = findViewById<Button>(R.id.buttonBackFromSettings)
-
+        val buttonBackFromSettings = findViewById<TextView>(R.id.buttonBackFromSettings)
         buttonBackFromSettings.setOnClickListener{
             finish()
         }
