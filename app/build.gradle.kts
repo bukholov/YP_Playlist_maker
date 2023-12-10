7b66f5b5301c30c1723d1f8ec82ee36e65790b5e
bukholovds <bukholovds@gmail.com>
2023-12-10 16:18:22 +0300

    Домашнее задание спринта №11. Работа с сетью * Добавил формы заглушки для результатов поиска, когда ничего не найдено и когда нет соединения с сетью; * Добавил интерфейс для работы с сетью; * В классе Track поменял тип данных у длительноси трека на Int; * В TrackViewHolder добавил перевод из милисекунд в минуты; * В SearchActivity вынес поиск запроса в отдельный метод; * Создал отдельную переменную для сохранения последнего поискового запроса, чтобы при повторе попытке был текст старого запроса;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,9 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
@@ -60,13 +63,18 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.google.code.gson:gson:2.10")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
